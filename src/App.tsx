1ab115d4837a4f6421c6de31d9e535fcea8fba4f
alexander-vecher <evening.sasha@mail.ru>
2022-03-09 22:33:02 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import './App.css';
-import {NewComponent} from "./NewComponent";
 
 
-function App() {
-
+export function App() {
+    /*const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+        console.log('Hellow Im VASYA!')
+    }
+    const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+        console.log('Hellow Im IVAN!')
+    }*/
+    const onClickHandler=(name:string)=>{
+        console.log(name)
+    }
     return (
+        <div className="App">
+            <button onClick={()=>onClickHandler('VASYA')}>MyYouTubeChanel-1</button>
+            <button onClick={()=>onClickHandler('IVAN')}>MyYouTubeChanel-2</button>
 
-        <NewComponent/>
 
-    );
+        </div>
+    )
 }
 
-export default App;
