60e4f546167e16f39164892f0923f5859da38512
alexander-vecher <evening.sasha@mail.ru>
2022-03-20 22:31:12 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ export function App() {
         {banknots: 'Dollars ', value: 50, number: ' x1234567890'},
         {banknots: 'RUBLS ', value: 50, number: ' v1234567890'},
     ])
-let currentMoney= money.filter((filteredMoney)=>filteredMoney.banknots==='RUBLS ')
+    let currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS ')
 
     return (
         <ul>
             {currentMoney.map((objFromMoneyArr, index) => {
-                return (<li key={index} >
+                return (<li key={index}>
                         <span>{objFromMoneyArr.banknots}</span>
                         <span>{objFromMoneyArr.value}</span>
                         <span>{objFromMoneyArr.number}</span>
