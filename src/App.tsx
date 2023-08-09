f65e703302161ed65f9b702d7537a2e5b4f4473b
Alexander <evening.sasha@mail.ru>
2023-08-09 17:51:22 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Button} from "./components/Button";
+
+/*import {Button} from "./components/Button";*/
 
 function App() {
 
-    return (
-        <div className="App">
 
-            <Button name={'MyYouTubeChanel-1'}/>
-            <Button name={'MyYouTubeChanel-2'}/>
+    let[a,setA] = useState(1)
 
 
+    const onClickHundler = () => {
+        setA(++a)
+        /*console.log(a)*/
+    }
+    const onClickHundler2 = () => {
+        setA(a=0)
+        /*console.log(a)*/
+    }
+
+    return (<div>
+            <h1>
+                {a}
+            </h1>
+            <button onClick={onClickHundler}>number</button>
+            <button onClick={onClickHundler2}>0</button>
         </div>
     );
 }
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-type NameProps = {
-    name: string
-}
-
-export const Button = (props: NameProps) => {
-    return (
-        <button>"{props.name}</button>
-    )
-}
