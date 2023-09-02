d74f4919357f1ae0db6af5dd9d952b5cb4a6b4dc
Alexander <evening.sasha@mail.ru>
2023-09-02 20:16:20 +0300

    double

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,40 +1,28 @@
 import React from "react";
+import { currentMoney, onClickFilter } from "./App";
 
-/*type NewComponentType = {
-    AAA?: StudentType[]
+type Current = {
+    MMmoney: onClickFilter
+    Mmoney:currentMoney;
 }
-
-type topCars= {
-    Cars: CarsType[]}
-
-    type CarsType={
-    manufacturer: string
-    model: string
-}
-
-type StudentType = {
-    id: number,
-    name: string,
-    age: number
-}*/
-
-export const NewComponent = (/*props: topCars*/) => {
-
-    const topCars = [
-        {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'},
-        {manufacturer: 'Audi', model: 'rs6'}
-    ]
-
+export const NewComponent = (props:Current) => {
     return (
-        <table>
-            {topCars.map((el, index) => {
-                return (
-                    <tr key={index}>
-                        <th>{index+1}</th>
-                        <td>{el.manufacturer}</td>
-                        <td>{el.model}</td>
-                    </tr>)
-            })}
-        </table>
-    )}
+        <>
+            <ul>
+                {props.Mmoney.map((objFromMoneyArr, index) => {
+                        return (
+                            <li key={index}>
+                                <span> {objFromMoneyArr.banknots}</span>
+                                <span> {objFromMoneyArr.value}</span>
+                                <span> {objFromMoneyArr.number}</span>
+                            </li>
+                        )
+                    }
+                )}
+            </ul>
+            <button onClick={() => props.MMmoney('all')}>all</button>
+            <button onClick={() => props.MMmoney('ruble')}>ruble</button>
+            <button onClick={() => props.MMmoney('dollars')}>dollar</button>
+        </>
+    )
+}
